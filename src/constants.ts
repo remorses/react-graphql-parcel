3964d83cc8c9b09ba6fab144c41fb32605c04913
remorses <beats.by.morse@gmail.com>
2019-11-16 19:14:41 +0100

    constants:

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ export const theme = {}
 
 export const LOCAL_STORAGE_JWT_TOKEN_KEY = 'token'
 
-export const GRAPHQL_PRODUCTION_ENDPOINT = 'http://localhost:8080'
+export const GRAPHQL_PRODUCTION_ENDPOINT = ''
 export const GRAPHQL_MOCKING_ENDPOINT = 'http://localhost:9000'
-export const GRAPHQL_TESTING_ENDPOINT = 'http://localhost:9000'
+export const GRAPHQL_TESTING_ENDPOINT = 'http://localhost:8080'
 export const mocking = Boolean(process.env.MOCKING)
