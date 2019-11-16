6e4d0bd6620eae7e5a214aaa5e4564813931963a
Tommaso De Rossi <beats.by.morse@gmail.com>
2019-11-16 14:46:43 +0100

    Initial commit

---
+++ config-overrides.js
@@ -0,0 +1,36 @@
+const rewireReactHotLoader = require('react-app-rewire-hot-loader')
+const path = require('path')
+const { override, useEslintRc, fixBabelImports, disable } = require('customize-cra')
+
+
+module.exports = override(
+    // useEslintRc(path.resolve(__dirname, '.eslintrc')),
+    disableEsLint(),
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: 'css'
+    }),
+    (config, env) => {
+        config = rewireReactHotLoader(config, env)
+        return config
+    }
+)
+
+// module.exports = function override(config, env) {
+//     config = rewireEslint(config, env)
+//     if (env === 'development') {
+//         config.resolve.alias['react-dom'] = '@hot-loader/react-dom'
+//     }
+//     config.plugins.push(
+//         new webpack.EnvironmentPlugin({
+//             LATER_COV: false // fixes problem with later.js and prettycron
+//         })
+//     )
+//     config.resolve.alias['@ant-design/icons/lib/dist$'] = path.resolve(
+//         __dirname,
+//         './src/images/icons.js'
+//     )
+//     config = rewireReactHotLoader(config, env)
+//     return config
+// }
